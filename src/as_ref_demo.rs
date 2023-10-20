94265e1b569b4718abf74dc606fc7a05686bcc8e
Yonghao Shu <123shuyonghao@gmail.com>
2023-10-19 22:57:25 -0700

    AsRef demo mittsu

--- src/as_ref_demo.rs
+++ src/as_ref_demo.rs
@@ -7,6 +7,13 @@ mod test {
         println!("s1 = {}, s2 = {}", s1, s2);
     }
 
+    #[test]
+    fn as_ref_demo_futatsu() {
+        let integer_wrapper = IntegerWrapper { value: 42 };
+        print_i32(integer_wrapper.as_ref());
+        print_value(integer_wrapper);
+    }
+
     struct IntegerWrapper {
         value: i32,
     }
@@ -25,14 +32,37 @@ mod test {
         println!("val = {}", val);
     }
 
-    fn p(s: &str) {
-        println!("s = {}", s);
+    #[test]
+    fn as_ref_demo_mittsu() {
+        let t1 = String::from("Hello");
+        print_str_ref(t1);
+
+        let t2 = IntStrWrapper::new(42);
+        print_str_ref(t2);
     }
 
-    #[test]
-    fn as_ref_demo_futatsu() {
-        let integer_wrapper = IntegerWrapper { value: 42 };
-        print_i32(integer_wrapper.as_ref());
-        print_value(integer_wrapper);
+    struct IntStrWrapper {
+        val: i32,
+        val_holder: String,
+    }
+
+    impl IntStrWrapper {
+        fn new(val: i32) -> Self {
+            Self { 
+                val,
+                val_holder: String::from(val.to_string()),
+            }
+        }
+    }
+
+    impl AsRef<str> for IntStrWrapper {
+        fn as_ref(&self) -> &str {
+            &self.val_holder
+        }
+    }
+
+    fn print_str_ref<T: AsRef<str>>(t: T) {
+        let t_ref = t.as_ref();
+        println!("t_ref = {}", t_ref);
     }
 }
