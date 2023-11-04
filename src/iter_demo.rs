4e24c249b964d8fe73d351964920a134e4e77dca
Yonghao Shu <123shuyonghao@gmail.com>
2023-11-04 02:10:40 -0700

    Iter demo: for loop

--- src/iter_demo.rs
+++ src/iter_demo.rs
@@ -17,4 +17,19 @@ mod test {
             }
         }
     }
+
+    #[test]
+    fn for_int_vec_demo() {
+        let numbers = vec![1, 3, 5, 4, 7, 8, 9];
+
+        for number in numbers {
+            println!("First round: {}", number);
+        }
+
+        // We cannot use numbers again because it has been moved into the for loop,
+        // although the number type is i32 which is Copy trait.
+        // for number in numbers {
+        //     println!("Second round: {}", number);
+        // }
+    }
 }
