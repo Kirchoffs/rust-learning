c5eb6cfde6774ed86ccc1924184606856ac9e441
Yonghao Shu <123shuyonghao@gmail.com>
2023-11-23 23:35:19 -0800

    Cursor read / write demo

--- src/cursor_demo.rs
+++ src/cursor_demo.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod test {
-    use std::io::{self, Write, Seek, SeekFrom};
+    use std::io::{self, Write, Read, Seek, SeekFrom};
     use std::io::Cursor;
 
     fn write_ten_bytes_at_end<W: Write + Seek>(mut writer: W) -> io::Result<()> {
@@ -19,5 +19,30 @@ mod test {
         let mut buff = Cursor::new(vec![0; 15]);
         write_ten_bytes_at_end(&mut buff).unwrap();
         assert_eq!(&buff.get_ref()[5..15], &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    }
+
+    #[test]
+    fn cursor_write_demo() {
+        let mut buffer = Cursor::new(Vec::new());
+
+        buffer.write_all(b"Hello, world!").unwrap();
+    
+        let written_data = buffer.into_inner();
+    
+        let written_string = String::from_utf8(written_data).unwrap();
+        assert_eq!(written_string, "Hello, world!");
+    }
+
+    #[test]
+    fn cursor_read_demo() {
+        let data = b"Hello, world!";
+        let mut cursor = Cursor::new(data);
+    
+        let mut buffer = Vec::new();
+    
+        cursor.read_to_end(&mut buffer).unwrap();
+    
+        let read_string = String::from_utf8(buffer).unwrap();
+        assert_eq!(read_string, "Hello, world!");
     }
 }
