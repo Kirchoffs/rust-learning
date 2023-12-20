3e75e7c4740054aedd69ba015891d3af1a2f5929
Yonghao Shu <123shuyonghao@gmail.com>
2023-12-20 01:34:30 -0800

    Vec of integer demo

--- src/vec_demo.rs
+++ src/vec_demo.rs
@@ -7,4 +7,50 @@ mod test {
         let e = &v[1];
         println!("e = {}", e);
     }
+
+    #[test]
+    fn integer_vec_demo_alpha() {
+        let v = vec![1, 2, 3, 4, 5];
+
+        for e in v.iter() {
+            println!("e = {}", e + 42);
+        }
+
+        for &e in v.iter() {
+            println!("e = {}", e + 42);
+        }
+
+        // v is moved here
+        for e in v.into_iter() {
+            println!("e = {}", e + 42);
+        }
+
+        // Wrong usage: use of moved value: v
+        // for e in v {
+        //     println!("e = {}", e + 42);
+        // }
+    }
+
+    #[test]
+    fn integer_vec_demo_beta() {
+        let v = vec![1, 2, 3, 4, 5];
+
+        for e in v.iter() {
+            println!("e = {}", e + 42);
+        }
+
+        for &e in v.iter() {
+            println!("e = {}", e + 42);
+        }
+
+        // v is moved here
+        for e in v {
+            println!("e = {}", e + 42);
+        }
+
+        // Wrong usage: use of moved value: v
+        // for e in v.into_iter() {
+        //     println!("e = {}", e + 42);
+        // }
+    }
 }
